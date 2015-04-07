ffbb8459ae173d648c4d25674fdf1bd575602a47
JohnLui <wenhanlv@gmail.com>
2015-04-07 20:35:36 +0800

    更新Simditor，增加 gif 支持，增加图片尺寸设置和查看源代码功能。

--- johnlui_markdown_html.php
+++ johnlui_markdown_html.php
@@ -1,54 +1,49 @@
-<link rel="stylesheet" type="text/css" href="<?php echo BLOG_URL; ?>/content/plugins/johnlui_markdown/styles/font-awesome.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo BLOG_URL; ?>/content/plugins/johnlui_markdown/styles/simditor.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo BLOG_URL; ?>content/plugins/johnlui_markdown/styles/simditor.css" />
 
 <script type="text/javascript" src="http://libs.baidu.com/jquery/2.1.1/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo BLOG_URL; ?>/content/plugins/johnlui_markdown/scripts/js/module.js"></script>
-<script type="text/javascript" src="<?php echo BLOG_URL; ?>/content/plugins/johnlui_markdown/scripts/js/simditor.min.js"></script>
-<script type="text/javascript" src="<?php echo BLOG_URL; ?>/content/plugins/johnlui_markdown/scripts/js/simditor-markdown.js"></script>
+<script type="text/javascript" src="<?php echo BLOG_URL; ?>content/plugins/johnlui_markdown/scripts/js/module.min.js"></script>
+<script type="text/javascript" src="<?php echo BLOG_URL; ?>content/plugins/johnlui_markdown/scripts/js/hotkeys.js"></script>
+<script type="text/javascript" src="<?php echo BLOG_URL; ?>content/plugins/johnlui_markdown/scripts/js/simditor.min.js"></script>
+<script type="text/javascript" src="<?php echo BLOG_URL; ?>content/plugins/johnlui_markdown/scripts/js/simditor-markdown.js"></script>
 
 <script>
 var markdown_editor = '<textarea id="content" name="content" placeholder="输入内容，文章最后记得加回车哦~" autofocus></textarea>';
-// var markdown_editor_excerpt = '<textarea id="excerpt" name="excerpt" placeholder="输入摘要，记得在最后加回车哦~"></textarea>';
 
 $(function() {
   var val = $('#content').val();
   $('.ke-container').remove();
   $('#content').parent().remove();
-  // $('#excerpt').parent().html(markdown_editor_excerpt);
   $('#post_bar').css('width', '847px');
   $('#FrameUpload').after(markdown_editor);
 
-  // var editors = ['content', 'excerpt'];
-  // for (var i = 0; i < editors.length; i++) {
-    // var editor = editors[i];
-    var editor = new Simditor({
-      textarea: $('#content'),
-      defaultImage: '/content/plugins/johnlui_markdown/images/image.png',
-      toolbar: [
-        'title',
-        'bold',
-        'italic',
-        'underline',
-        'strikethrough',
-        'color',
-        'ol',
-        'ul',
-        'blockquote',
-        'code',
-        'table',
-        'link',
-        'image',
-        'hr',
-        'indent',
-        'outdent'
-        ],
-      markdown: true,
-    });
-    editor.setValue(val);
-    editor.on('valuechanged', function(){
-      editor.sync();
-    });
-  // }
+  var editor = new Simditor({
+    textarea: $('#content'),
+    defaultImage: '/content/plugins/johnlui_markdown/images/image.png',
+    toolbar: [
+      'title',
+      'bold',
+      'italic',
+      'underline',
+      'strikethrough',
+      'color',
+      'ol',
+      'ul',
+      'blockquote',
+      'code',
+      'table',
+      'link',
+      'image',
+      'hr',
+      'indent',
+      'outdent',
+      'source'
+      ],
+    markdown: true,
+  });
+  editor.setValue(val);
+  editor.on('valuechanged', function(){
+    editor.sync();
+  });
   $('.simditor-placeholder').css('top', 'auto');
 });
 </script>
--- scripts/js/simditor-markdown.js
+++ scripts/js/simditor-markdown.js
@@ -1,213 +1,229 @@
-(function() {
-  var Markdown,
-    __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    __slice = [].slice;
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define('simditor-markdown', ["jquery","simple-module","simditor"], function ($, SimpleModule, Simditor) {
+      return (root['SimditorMarkdown'] = factory($, SimpleModule, Simditor));
+    });
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require("jquery"),require("simple-module"),require("simditor"));
+  } else {
+    root['SimditorMarkdown'] = factory(jQuery,SimpleModule,Simditor);
+  }
+}(this, function ($, SimpleModule, Simditor) {
 
-  Markdown = (function(_super) {
-    __extends(Markdown, _super);
+var SimditorMarkdown,
+  extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+  hasProp = {}.hasOwnProperty;
 
-    Markdown.prototype.opts = {
-      markdown: false
-    };
+SimditorMarkdown = (function(superClass) {
+  extend(SimditorMarkdown, superClass);
 
-    function Markdown() {
-      var args;
-      args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-      Markdown.__super__.constructor.apply(this, args);
-      this.editor = this.widget;
+  function SimditorMarkdown() {
+    return SimditorMarkdown.__super__.constructor.apply(this, arguments);
+  }
+
+  SimditorMarkdown.pluginName = 'Markdown';
+
+  SimditorMarkdown.prototype.opts = {
+    markdown: false
+  };
+
+  SimditorMarkdown.prototype._init = function() {
+    var hooks;
+    if (!this.opts.markdown) {
+      return;
     }
-
-    Markdown.prototype._init = function() {
-      var hooks;
-      if (!this.opts.markdown) {
-        return;
-      }
-      if (typeof this.opts.markdown === "object") {
-        hooks = $.extend({}, this.hooks, this.opts.markdown);
-      } else {
-        hooks = $.extend({}, this.hooks);
-      }
-      return this.editor.on("keypress", (function(_this) {
-        return function(e) {
-          var $blockEl, button, cmdEnd, cmdStart, container, content, hook, match, name, range, result, testRange;
-          if (!(e.which === 32 || e.which === 13)) {
+    this.editor = this._module;
+    if (typeof this.opts.markdown === "object") {
+      hooks = $.extend({}, this.hooks, this.opts.markdown);
+    } else {
+      hooks = $.extend({}, this.hooks);
+    }
+    return this.editor.on("keypress", (function(_this) {
+      return function(e) {
+        var $blockEl, button, cmdEnd, cmdStart, container, content, hook, match, name, range, result, testRange;
+        if (!(e.which === 32 || e.which === 13)) {
+          return;
+        }
+        range = _this.editor.selection.getRange();
+        container = range != null ? range.commonAncestorContainer : void 0;
+        if (!(range && range.collapsed && container && container.nodeType === 3 && !$(container).parent("pre").length)) {
+          return;
+        }
+        content = container.textContent;
+        for (name in hooks) {
+          hook = hooks[name];
+          if (e.which === 13 && !hook.enterKey) {
             return;
           }
-          range = _this.editor.selection.getRange();
-          container = range != null ? range.commonAncestorContainer : void 0;
-          if (!(range && range.collapsed && container && container.nodeType === 3 && !$(container).parent("pre").length)) {
-            return;
+          if (!(hook && hook.cmd instanceof RegExp)) {
+            continue;
           }
-          content = container.textContent;
-          for (name in hooks) {
-            hook = hooks[name];
-            if (e.which === 13 && !hook.enterKey) {
-              return;
-            }
-            if (!(hook && hook.cmd instanceof RegExp)) {
-              continue;
-            }
-            match = content.match(hook.cmd);
-            if (!match) {
-              continue;
-            }
-            button = _this.editor.toolbar.findButton(name);
-            if (button === null || button.disabled) {
-              continue;
-            }
-            if (hook.block) {
-              $blockEl = _this.editor.util.closestBlockEl(container);
-              testRange = document.createRange();
-              testRange.setStart(container, 0);
-              testRange.collapse(true);
-              if (!_this.editor.selection.rangeAtStartOf($blockEl, testRange)) {
-                continue;
-              }
-            }
-            cmdStart = match.index;
-            cmdEnd = match[0].length + match.index;
-            range.setStart(container, cmdStart);
-            range.setEnd(container, cmdEnd);
-            if (hook.block) {
-              range.deleteContents();
-              if (_this.editor.util.isEmptyNode($blockEl)) {
-                $blockEl.append(_this.editor.util.phBr);
-              }
-              _this.editor.selection.setRangeAtEndOf($blockEl);
-            }
-            result = hook.callback.call(_this, button, hook, range, match, $blockEl);
-            if ((e.which === 32 || name === "code") && result) {
-              e.preventDefault();
-            }
-            break;
+          match = content.match(hook.cmd);
+          if (!match) {
+            continue;
           }
-        };
-      })(this));
-    };
+          button = _this.editor.toolbar.findButton(name);
+          if (button === null || button.disabled) {
+            continue;
+          }
+          if (hook.block) {
+            $blockEl = _this.editor.util.closestBlockEl(container);
+            testRange = document.createRange();
+            testRange.setStart(container, 0);
+            testRange.collapse(true);
+            if (!_this.editor.selection.rangeAtStartOf($blockEl, testRange)) {
+              continue;
+            }
+          }
+          cmdStart = match.index;
+          cmdEnd = match[0].length + match.index;
+          range.setStart(container, cmdStart);
+          range.setEnd(container, cmdEnd);
+          if (hook.block) {
+            range.deleteContents();
+            if (_this.editor.util.isEmptyNode($blockEl)) {
+              $blockEl.append(_this.editor.util.phBr);
+            }
+            _this.editor.selection.setRangeAtEndOf($blockEl);
+          }
+          result = hook.callback.call(_this, button, hook, range, match, $blockEl);
+          if ((e.which === 32 || name === "code") && result) {
+            e.preventDefault();
+          }
+          break;
+        }
+      };
+    })(this));
+  };
 
-    Markdown.prototype.hooks = {
-      title: {
-        cmd: /^#+/,
-        block: true,
-        enterKey: true,
-        callback: function(button, hook, range, match, $blockEl) {
-          var level;
-          level = Math.min(match[0].length, 3);
-          return button.command("h" + level);
-        }
-      },
-      blockquote: {
-        cmd: /^>{1}/,
-        block: true,
-        enterKey: true,
-        callback: function(button, hook, range, match, $blockEl) {
-          return button.command();
-        }
-      },
-      code: {
-        cmd: /^`{3}/,
-        block: true,
-        enterKey: true,
-        callback: function(button, hook, range, match, $blockEl) {
-          return button.command();
-        }
-      },
-      hr: {
-        cmd: /^\*{3,}$|^\-{3,}$/,
-        block: true,
-        enterKey: true,
-        callback: function(button, hook, range, match, $blockEl) {
-          return button.command();
-        }
-      },
-      bold: {
-        cmd: /\*{2}([^\*]+)\*{2}$|_{2}([^_]+)_{2}$/,
-        block: false,
-        callback: function(button, hook, range, match) {
-          var text, textNode;
-          text = match[1] || match[2];
-          textNode = document.createTextNode(text);
-          this.editor.selection.selectRange(range);
-          range.deleteContents();
-          range.insertNode(textNode);
-          range.selectNode(textNode);
-          this.editor.selection.selectRange(range);
-          document.execCommand("bold");
-          this.editor.selection.setRangeAfter(textNode);
-          document.execCommand("bold");
-          this.editor.trigger("valuechanged");
-          return this.editor.trigger("selectionchanged");
-        }
-      },
-      italic: {
-        cmd: /\*([^\*]+)\*$/,
-        block: false,
-        callback: function(button, hook, range, match) {
-          var text, textNode;
-          text = match[1] || match[2];
-          textNode = document.createTextNode(text);
-          this.editor.selection.selectRange(range);
-          range.deleteContents();
-          range.insertNode(textNode);
-          range.selectNode(textNode);
-          this.editor.selection.selectRange(range);
-          document.execCommand("italic");
-          this.editor.selection.setRangeAfter(textNode);
-          document.execCommand("italic");
-          this.editor.trigger("valuechanged");
-          return this.editor.trigger("selectionchanged");
-        }
-      },
-      ul: {
-        cmd: /^\*{1}$|^\+{1}$|^\-{1}$/,
-        block: true,
-        callback: function(button, hook, range, match, $blockEl) {
-          return button.command();
-        }
-      },
-      ol: {
-        cmd: /^[0-9][\.\u3002]{1}$/,
-        block: true,
-        callback: function(button, hook, range, match, $blockEl) {
-          return button.command();
-        }
-      },
-      image: {
-        cmd: /!\[(.+)\]\((.+)\)$/,
-        block: true,
-        callback: function(button, hook, range, match) {
-          return button.command(match[2]);
-        }
-      },
-      link: {
-        cmd: /\[(.+)\]\((.+)\)$|\<((.[^\[\]\(\)]+))\>$/,
-        block: false,
-        callback: function(hook, range, match) {
-          var $link, url;
-          url = match[2] || match[4];
-          if (!/[a-zA-z]+:\/\/[^\s]*/.test(url)) {
-            return false;
-          }
-          $link = $("<a/>", {
-            text: match[1] || match[3],
-            href: url,
-            target: "_blank"
-          });
-          this.editor.selection.selectRange(range);
-          range.deleteContents();
-          range.insertNode($link[0]);
-          this.editor.selection.setRangeAfter($link);
-          this.editor.trigger("valuechanged");
-          return this.editor.trigger("selectionchanged");
-        }
+  SimditorMarkdown.prototype.hooks = {
+    title: {
+      cmd: /^#+/,
+      block: true,
+      enterKey: true,
+      callback: function(button, hook, range, match, $blockEl) {
+        var level;
+        level = Math.min(match[0].length, 3);
+        return button.command("h" + level);
       }
-    };
+    },
+    blockquote: {
+      cmd: /^>{1}/,
+      block: true,
+      enterKey: true,
+      callback: function(button, hook, range, match, $blockEl) {
+        return button.command();
+      }
+    },
+    code: {
+      cmd: /^`{3}/,
+      block: true,
+      enterKey: true,
+      callback: function(button, hook, range, match, $blockEl) {
+        return button.command();
+      }
+    },
+    hr: {
+      cmd: /^\*{3,}$|^\-{3,}$/,
+      block: true,
+      enterKey: true,
+      callback: function(button, hook, range, match, $blockEl) {
+        return button.command();
+      }
+    },
+    bold: {
+      cmd: /\*{2}([^\*]+)\*{2}$|_{2}([^_]+)_{2}$/,
+      block: false,
+      callback: function(button, hook, range, match) {
+        var text, textNode;
+        text = match[1] || match[2];
+        textNode = document.createTextNode(text);
+        this.editor.selection.selectRange(range);
+        range.deleteContents();
+        range.insertNode(textNode);
+        range.selectNode(textNode);
+        this.editor.selection.selectRange(range);
+        document.execCommand("bold");
+        this.editor.selection.setRangeAfter(textNode);
+        document.execCommand("bold");
+        this.editor.trigger("valuechanged");
+        return this.editor.trigger("selectionchanged");
+      }
+    },
+    italic: {
+      cmd: /\*([^\*]+)\*$/,
+      block: false,
+      callback: function(button, hook, range, match) {
+        var text, textNode;
+        text = match[1] || match[2];
+        textNode = document.createTextNode(text);
+        this.editor.selection.selectRange(range);
+        range.deleteContents();
+        range.insertNode(textNode);
+        range.selectNode(textNode);
+        this.editor.selection.selectRange(range);
+        document.execCommand("italic");
+        this.editor.selection.setRangeAfter(textNode);
+        document.execCommand("italic");
+        this.editor.trigger("valuechanged");
+        return this.editor.trigger("selectionchanged");
+      }
+    },
+    ul: {
+      cmd: /^\*{1}$|^\+{1}$|^\-{1}$/,
+      block: true,
+      callback: function(button, hook, range, match, $blockEl) {
+        return button.command();
+      }
+    },
+    ol: {
+      cmd: /^[0-9][\.\u3002]{1}$/,
+      block: true,
+      callback: function(button, hook, range, match, $blockEl) {
+        return button.command();
+      }
+    },
+    image: {
+      cmd: /!\[(.+)\]\((.+)\)$/,
+      block: true,
+      callback: function(button, hook, range, match) {
+        return button.command(match[2]);
+      }
+    },
+    link: {
+      cmd: /\[(.+)\]\((.+)\)$|\<((.[^\[\]\(\)]+))\>$/,
+      block: false,
+      callback: function(hook, range, match) {
+        var $link, url;
+        url = match[2] || match[4];
+        if (!/[a-zA-z]+:\/\/[^\s]*/.test(url)) {
+          return false;
+        }
+        $link = $("<a/>", {
+          text: match[1] || match[3],
+          href: url,
+          target: "_blank"
+        });
+        this.editor.selection.selectRange(range);
+        range.deleteContents();
+        range.insertNode($link[0]);
+        this.editor.selection.setRangeAfter($link);
+        this.editor.trigger("valuechanged");
+        return this.editor.trigger("selectionchanged");
+      }
+    }
+  };
 
-    return Markdown;
+  return SimditorMarkdown;
 
-  })(Plugin);
+})(SimpleModule);
 
-  Simditor.connect(Markdown);
+Simditor.connect(SimditorMarkdown);
 
-}).call(this);
+return SimditorMarkdown;
+
+}));
